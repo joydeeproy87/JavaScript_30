89f46ba5c468e8615fc1623001ec9f617321c98f
joydeeproy87 <mail2joydeep.roy@gmail.com>
2017-12-18 23:37:10 +0530

    fifth week

--- 11-Custom_Video_Player/app.js
+++ 11-Custom_Video_Player/app.js
@@ -1,8 +1,21 @@
 // get our Elements
+const player = document.querySelector('.player');
+
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const ranges = player.querySelectorAll('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+
+console.log(player, video, progress, progressBar, toggle, skipButtons, ranges);
 
 
 // build out functions
-
+function togglePlay () {
+	const method = video.paused ? 'play' : 'pause';
+	video[method]();
+}
 
 // Hook up the event listener
 
