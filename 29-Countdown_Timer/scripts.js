0b507430e3b537aad4bcd40b77bdc880bb3ae2e8
joydeeproy87 <mail2joydeep.roy@gmail.com>
2018-01-20 13:01:12 +0530

    ninth week

--- 29-Countdown_Timer/scripts.js
+++ 29-Countdown_Timer/scripts.js
@@ -1,4 +1,15 @@
-function timer (seconds) {
+let countDown;
+
+function timer(seconds) {
 	const now = Date.now();
 	const then = now + seconds * 1000;
+
+	countDown = setInterval(() => {
+		const secondsLeft = Math.round((then - Date.now()) / 1000);
+		console.log(secondsLeft);
+		if (secondsLeft < 0) {
+			clearInterval(countDown);
+			return;
+		}
+	}, 1000);
 }
